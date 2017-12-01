018dfda285f8c9fbea70f66c99786003dfa37dfb
Benjamin Poon <benjamintpoon@gmail.com>
2017-12-01 02:16:56 -0600

    Implement authentication on the backend

---
+++ api/lib/slek/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule Slek.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias Slek.Repo
+  alias Slek.User
+
+  def for_token(user = %User{}), do: {:ok, "User:#{user.id}" }
+  def for_token(_), do: {:error, "Unknown resource type"}
+
+  def from_token("User:" <> id), do: {:ok, Repo.get(User, String.to_integer(id))}
+  def from_token(_), do: {:error, "Unknown resouce type"}
+end
--- api/web/router.ex
+++ api/web/router.ex
@@ -1,8 +1,33 @@
 defmodule Slek.Router do
   use Slek.Web, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  scope "/api", Slek do
+    pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionConroller, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
   end
 
   scope "/api", Slek do
